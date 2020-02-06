c2aa1d7c8c9b9816a1c879adae79cf816e61cf3f
szymek1922 <44381046+szymek1922@users.noreply.github.com>
2020-02-06 11:56:14 +0100

    Add files via upload

---
+++ BusAplication/Model/Bus.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+public class Bus : INotifyPropertyChanged
+{
+    private int busId;
+    private string name;
+    private string kierowca;
+
+    public int BusId
+    {
+        get
+        {
+            return busId;
+        }
+        set
+        {
+            busId = value;
+            OnPropertyChanged("BusId");
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+            OnPropertyChanged("Name");
+        }
+    }
+    public string Kierowca
+    {
+        get
+        {
+            return kierowca;
+        }
+        set
+        {
+            kierowca = value;
+            OnPropertyChanged("Kierowca");
+        }
+    }
+    public List<string> Przystanek { get; set; }
+
+    #region INotifyPropertyChanged Members  
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    #endregion
+
+}
